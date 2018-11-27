ba32e57b1d1d238fd31dd0e8c7dded1ba91f5187
Grady Robbins <gradyrobbins13@gmail.com>
2018-11-27 15:28:26 -0600

    misc campaign.js

--- campaign.js
+++ campaign.js
@@ -36,30 +36,48 @@ district.textContent = "Tennessee's 5th congressional district!";
         outDiv.innerHTML += 
         `<li>  ${key} : ${platform[key]} </li> `;
     }
-
     
-// URL for donation form- can change the href dynamically via JS below:
-document.getElementById("donate_link").href = "https://www.nashville.gov/Election-Commission.aspx";
-
-// Calendar of events
-let calendarEl = document.getElementById("calendar");
-calendarEl.textContent += "Free beer tomorrow";
-
-// Volunteer information
-const volunteer = {
-    Name: [],
-    Address:[],
-    Email: [],
-    Phone_number: [],
-    Availability: []
-    // What activities each one is willing to do (e.g. answering phone calls, taking polls, etc.)
-}
     
-
-// Biography
-// Image gallery
-// Head shot
-// Picture of family
-// Picture of constituents
-// Mission statement
-// URL for registering to vote 
+    // URL for donation form- can change the href dynamically via JS below:
+    document.getElementById("donate_link").href = "https://www.nashville.gov/Election-Commission.aspx";
+    
+    // Calendar of events
+    let calendarEl = document.getElementById("calendar");
+    calendarEl.textContent += "Free beer tomorrow";
+    
+    // Volunteer information
+    const volunteer = {
+        Id: [],
+        Name: [],
+        Address:[],
+        Email: [],
+        Phone_number: [],
+        Availability: []
+        // What activities each one is willing to do (e.g. answering phone calls, taking polls, etc.)
+    }
+    
+    volunteer.Id.push(1, 2, 3);
+    volunteer.Name.push("Grady ", " Shu ", " Dejan");
+    volunteer.Phone_number.push("1-800-555-5555 ", " 615-999-8769 ", " 615-876-0909");
+    console.log(volunteer.Name);
+    volunteer.Email.push("bloop@aol.com ", " Bleep@hotmail.com ", " blip@msn.com");
+    console.table(volunteer);
+    
+    
+        
+    let volunteerDiv = document.querySelector("#volunteerinfo");
+        
+        for(key in volunteer){
+            x = volunteer[key];
+            console.log(key, " : ", volunteer[key]);
+            
+            volunteerDiv.innerHTML += 
+            `<li>  ${key} : ${volunteer[key]} </li> `;
+        }
+    // Biography
+    // Image gallery
+    // Head shot
+    // Picture of family
+    // Picture of constituents
+    // Mission statement
+    // URL for registering to vote 

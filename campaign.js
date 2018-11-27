f71c6357b9b586496d715c01b4207bfa5bf749b6
Grady Robbins <gradyrobbins13@gmail.com>
2018-11-27 10:27:00 -0600

    working on adding candidate's platform to DOM

--- campaign.js
+++ campaign.js
@@ -19,14 +19,71 @@ district.textContent = "Tennessee's 5th congressional district!";
     // Health care
     // Crime and enforcement
 
-const platform = {
-    Taxes: "Lorem Ipsum",
-    Jobs: "Lorem Ipsum",
-    Infrastructure: "Lorem Ipsum",
-    Healthcare: "Lorem Ipsum",
-    Crime_and_enforcement: "Lorem Ipsum",
-};
+    const platform = {
+        Taxes: "Lorem Ipsum Tax",
+        Jobs: "Lorem Ipsum Jobbie Job Job",
+        Infrastructure: "Lorem Ipsum Bridges and stuff",
+        Healthcare: "Lorem Ipsum drink water",
+        Crime_and_enforcement: "Lorem Ipsum judge judy",
+    };
+    
+    let outDiv = document.querySelector("#platform");
+    
+    for(key in platform){
+        x = platform[key];
+        console.log(key, " : ", platform[key]);
+        
+        outDiv.innerHTML += 
+        `<p>  Taxes: ${x.taxes}, Jobs: ${x.Jobs}, Infrastructure: ${x.Infrastructure} 
+         </p>`;
+    }
 
+    
+    // let students = [
+        //     {
+//         name: "terrance",
+//         track: "front-end", 
+//         achievements: 12,
+//         points: 6
+//     },
+//     {
+//         name: "juliet", 
+//         track: "back-end",
+//         achievements: 14,
+//         points: 7
+//     },
+//     {
+//         name: "Miller",
+//         track: "full-stack",
+//         achievements: 9,
+//         points: 7
+//     },
+//     {
+//         name: "Johnson", 
+//         track: "full-stack",
+//         achievements: 9,
+//         points: 5
+//     },
+//     {
+//         name: "Smith",
+//         track: "front-end",
+//         achievements: 9,
+//         points: 8
+//     }
+// ]
+// var message = "";
+// var student = "";
+
+// let outDiv = document.querySelector("#mine");
+
+// for(key in students){
+//     student=students[key];
+//     console.log(key, " : ", students[key]);
+    
+//     outDiv.innerHTML += 
+//     `<p> ${student.name}, Track: ${student.track}, Acieviments: ${student.achievements}, Points: ${student.points} 
+//      </p>`;
+// }
 
 // URL for donation form
 // Calendar of events
